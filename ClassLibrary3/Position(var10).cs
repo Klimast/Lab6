f802cb4da13f3a19ec669fe5b5db84767f6ffc1d
Klimast <150071377+Klimast@users.noreply.github.com>
2024-03-10 23:38:05 +0300

    Add files via upload
    добавил класс Должность вариант 10

---
+++ ClassLibrary3/Position(var10).cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ТРПО_Лаба_2_вариант_10__должность_
+{
+    class Position
+    {
+        private string title;
+        private double salary;
+        private string departmentLink;
+
+        public Position(string title, double salary, string departmentLink)
+        {
+            this.title = title;
+            this.salary = salary;
+            this.departmentLink = departmentLink;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public string DepartmentLink
+        {
+            get { return departmentLink; }
+            set { departmentLink = value; }
+        }
+    }
+}
